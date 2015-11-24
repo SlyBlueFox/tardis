90a43418c5913a3f441ae2aa3eddcff2574e30ee
SlyBlueFox <mdommell@gmail.com>
2015-11-24 16:20:26 -0500

    Tidied code into functions (find_show, find_wildcard)

--- tardis.py
+++ tardis.py
@@ -3,25 +3,41 @@ import csv
 import os
 import string
 
-
+# Compare text of 2 strings ignoring punctuation whitespace and numbers
 def compare(s1, s2):
         remove = string.punctuation + string.whitespace + string.digits
         return s1.translate(None, remove) == s2.translate(None, remove)
 
-array = list(csv.reader(open(r'test.csv')))
+# Scans for found video in provided list of known shows
+def find_show(video, csv):
+    for i, elmt in enumerate(csv):
+        if compare(video, csv[i][0]):
+            print(video, csv[i][0])
+            print('Show Found')
+            return i
+
+# Scans found show template for location of '##' wildcard
+def find_wildcard(templates, show_number):
+    return shows[show_number][0].index('##')
+
+# Reads csv into array for processing
+shows = list(csv.reader(open(r'test.csv')))
+
+# Scan directory for mkv files and load into array
 mkvs = []
 for root, dirs, files in os.walk("."):
     for scanned_file in files:
         if scanned_file.endswith('.mkv'):
             mkvs.append(scanned_file)
 
-for i, elmt in enumerate(array):
-    if compare(mkvs[0], array[i][0]):
-        print(mkvs[0], array[i][0])
-        print('Show Found')
-        show_index = i
-        print("Show Index: %d" % show_index)
-        print("Wildcard Location: %d" % array[i][0].index('##'))
-        '''lulz = array[i][0].replace('##', '02')
-        print(lulz)'''
+show_index = find_show(mkvs[0], shows)
+print("Show Index: %d" % show_index)
+
+show_wildcard = find_wildcard(shows, show_index)
+print("Wildcard Location: %d" % show_wildcard)
+
+'''
+        lulz = shows[i][0].replace('##', '02')
+        print(lulz)
         print(mkvs[0][28], mkvs[0][29])
+'''
